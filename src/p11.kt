0af3b88935c46138891158ece062132db8221503
Nishit079 <pnishit004@gmail.com>
2026-06-24 12:10:26 +0530

    Kotlin Practicals

---
+++ src/p11.kt
@@ -0,0 +1,30 @@
+fun main() {
+    var i = 10
+    var j: Int
+    j = 20
+    val k= 200
+
+    var a: Int = 22
+    var b: Float = 1.5f
+    var c: Char = 'T'
+    var d : String = "HMS"
+    var e : Boolean = false
+    var f : Double = 96.36
+    var g : Long = 338927847
+    var h : Short = -2
+    var n : Byte = 127
+
+    println("a = $a")
+    println("b = $b")
+    println("c = $c")
+    println("d = $d")
+    println("e = $e")
+    println("f = $f")
+    println("g = $g")
+    println("h = $h")
+    println("n = $n")
+
+    println("k= "+k)
+    println("j= "+j)
+    println("i ="+i)
+}
